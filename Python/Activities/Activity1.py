7c69a5134d380d59addbe6264328be7779d54758
Nagendrabalamamillapalli <130643277+Nagendrabalamamillapalli@users.noreply.github.com>
2023-06-27 17:53:46 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,9 @@
+import datetime
+
+name = input("Please Enter Your Name: ")
+age = input("Please enter your age:  ")
+year = ((2023 - int(age)) + 100)
+print(name + "  will be 100 year old in the year " + str(year) + ".")
+
+
+
