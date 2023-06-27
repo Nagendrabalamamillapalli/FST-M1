963a8b6cf1f6c1e4c21969d06b487cf94ec20b96
Nagendrabalamamillapalli <130643277+Nagendrabalamamillapalli@users.noreply.github.com>
2023-06-27 17:39:00 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -0,0 +1,15 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features",
+        glue = {"stepDefinitions"},
+        plugin = {"html: test-reports"},
+        monochrome = true
+)
+public class ActivitiesRunner {
+}
